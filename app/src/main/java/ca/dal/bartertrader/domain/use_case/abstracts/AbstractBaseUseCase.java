0f2835c8443a1b59839949c38b5574d8b3e39bd9
Urmzd Mukhammadnaim <hello@urmzd.com>
2026-03-15 21:03:38 -0500

    refactor(domain): remove unused imports from use cases
    Clean up unused imports from use case classes. Remove Task and
    QuerySnapshot imports from AbstractBaseUseCase that are not utilized.
    Remove unused imports from ResetPasswordUseCase, VerifyEmailExistsUseCase,
    SetPostUseCase, LoginUseCase, RegisterUseCase, and GetUserUseCase to
    improve code clarity.

--- app/src/main/java/ca/dal/bartertrader/domain/use_case/abstracts/AbstractBaseUseCase.java
+++ app/src/main/java/ca/dal/bartertrader/domain/use_case/abstracts/AbstractBaseUseCase.java
@@ -1,8 +1,5 @@
 package ca.dal.bartertrader.domain.use_case.abstracts;
 
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.QuerySnapshot;
-
 public abstract class AbstractBaseUseCase<RequestT, ResourceT> {
     public abstract ResourceT execute(RequestT request);
 }
--- app/src/main/java/ca/dal/bartertrader/domain/use_case/posts/SetPostUseCase.java
+++ app/src/main/java/ca/dal/bartertrader/domain/use_case/posts/SetPostUseCase.java
@@ -1,8 +1,5 @@
 package ca.dal.bartertrader.domain.use_case.posts;
 
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.QuerySnapshot;
-
 import ca.dal.bartertrader.data.repository.FirebasePostsRepository;
 import ca.dal.bartertrader.domain.model.PostModel;
 import ca.dal.bartertrader.domain.use_case.abstracts.AbstractBaseUseCase;
